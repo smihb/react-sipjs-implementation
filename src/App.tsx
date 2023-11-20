8a3a2821515a3d760b77946e4f056c1065f6b14f
Hayron Bomgnine <hayronbt@gmail.com>
2023-11-20 14:38:12 -0400

    add sipjs SimpleUser Connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
+import { Web } from 'sip.js'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  function getAudioElement(id: string): HTMLAudioElement {
+    const el = document.getElementById(id);
+    if (!(el instanceof HTMLAudioElement)) {
+      throw new Error(`Element "${id}" not found or not an audio element.`);
+    }
+    return el;
+  }
+
+  useEffect(() => {
+    const options: Web.SimpleUserOptions = {
+      aor: "", // caller
+      userAgentOptions: {
+        displayName: '',
+        authorizationUsername: '',
+        authorizationPassword: '',
+      },
+      media: {
+        constraints: { audio: true, video: false }, // audio only call
+        remote: { audio: getAudioElement("audioElement") } // play remote audio
+      }
+    };
+    const server = "";
+
+    const simpleUser = new Web.SimpleUser(server, options);
+
+
+    try {
+
+      simpleUser.connect()
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <audio controls id="audioElement"></audio>
+    </div>
   )
 }
 
